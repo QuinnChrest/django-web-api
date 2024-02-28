cc5951d6c427859613496defb7ee9ea6cfa03888
Quinn Chrest <quinnchr@gmail.com>
2024-02-27 22:58:52 -0600

    implement pagination for base search

--- WebAPI/recipes/views.py
+++ WebAPI/recipes/views.py
@@ -1,17 +1,37 @@
 import json
 from datetime import date, datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.forms.models import model_to_dict
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views import generic
 from django.utils import timezone
 
 from .models import Recipe
 
 def index(request):
-    data = Recipe.objects.all().values()
-    json_data = json.dumps(list(data), default=json_serial)
-    return HttpResponse(json_data, content_type='application/json')
+    recipes = Recipe.objects.all()
+    paginator = Paginator(recipes,1)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(paginator.num_pages)
+    data = [model_to_dict(recipe) for recipe in page_obj.object_list]
+    payload = {
+        "page": {
+            "current": page_obj.number,
+            "has_next": page_obj.has_next(),
+            "has_previous": page_obj.has_previous(),
+            "count": paginator.num_pages
+        },
+        "recipes": data
+    }
+    return JsonResponse(payload)
     
 def recipe(request, recipe_id):
     data = get_object_or_404(Recipe, pk=recipe_id)
